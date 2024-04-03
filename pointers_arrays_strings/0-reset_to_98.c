eb1ee31d4c718a93e90ee1a7e0071842d2b97b9c
cedric <604281+biteuljiouce@users.noreply.github.com>
2024-04-03 17:27:43 +0200

    fix doc and style according to Betty

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 /**
- * takes a pointer to an int as parameter and updates the value it points to to 98
- *
+ * reset_to_98 - Updates the value it points to to 98.
  * @n: integer pointer to update
  *
  * Return: always void
  */
- void reset_to_98(int *n) {
-    *n = 98;
+void reset_to_98(int *n)
+{
+*n = 98;
 }
