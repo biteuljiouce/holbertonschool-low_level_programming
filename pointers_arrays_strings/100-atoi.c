d256f24306902510c300f8036f6421274ee00233
cedric <604281+biteuljiouce@users.noreply.github.com>
2024-04-04 15:34:57 +0200

    ex11

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _atoi - Convert a string to an integer.
+ * @s: string to convert
+ *
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int num = 0;
+	int i;
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	for (i = 0; i <= len; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = (num * 10) + (s[i] - '0');
+		}
+		else if (num > 0)
+			break;
+	}
+
+	return (num * sign);
+}
