f0786fc3d02544bb1cee34574b3fb3ef3f314a9f
cedric <604281+biteuljiouce@users.noreply.github.com>
2024-04-04 09:08:29 +0200

    ex4

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string, followed by a new line, to stdout.
+ * @s: char pointer
+ *
+ * Return: int
+ */
+int _puts(char *s)
+{
+	return (puts(s));
+}
